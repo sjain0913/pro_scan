dde588f8e0137f6ec440d041a640eab61a99fc00
Saumya Jain <samjain0913@gmail.com>
2020-05-06 22:11:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,26 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Container(
-        padding: EdgeInsets.,
-        color: Colors.cyan,
-        child: Text('ay'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Text("one")
+          ),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.red,
+            child: Text("two"),
+          ),
+          Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.pink,
+              child: Text("three")
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
